f3e3983f24660c7db6bd113b1b0a8aa0bc1bd0f4
Muhammad Deril <49135753+derilkillms@users.noreply.github.com>
2023-03-08 10:35:19 +0700

    go-embed

---
+++ embed_test.go
@@ -0,0 +1,65 @@
+package golang_embed
+
+import (
+	"embed"
+	_ "embed"
+	"fmt"
+	"io/fs"
+	"os"
+	"testing"
+)
+
+//go:embed version.txt
+var version string
+
+func TestString(t *testing.T) {
+	fmt.Println(version)
+}
+
+//go:embed wau.png
+var wau []byte
+
+func TestByte(t *testing.T) {
+	err := os.WriteFile("wau_new.png", wau, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+//go:embed files/a.txt
+//go:embed files/b.txt
+//go:embed files/c.txt
+//go:embed files/d.txt
+
+var files embed.FS
+
+func TestMultipleFiles(t *testing.T) {
+	a, _ := files.ReadFile("files/a.txt")
+	fmt.Println(string(a))
+
+	b, _ := files.ReadFile("files/b.txt")
+	fmt.Println(string(b))
+
+	c, _ := files.ReadFile("files/c.txt")
+	fmt.Println(string(c))
+
+	d, _ := files.ReadFile("files/d.txt")
+	fmt.Println(string(d))
+}
+
+//go:embed files/*.txt
+var path embed.FS
+
+func TestPathMacher(t *testing.T) {
+	dirEntries, _ := path.ReadDir("files")
+	for _, entry := range dirEntries {
+		if !entry.IsDir() {
+			fmt.Println(entry.Name())
+			file, _ := path.ReadFile("files/" + entry.Name())
+			fmt.Println(string(file))
+
+		}
+	}
+
+}
